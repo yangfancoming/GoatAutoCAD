a8b34c8c9ddd0743d8ca35eae4c18bb402dee471
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-12-12 21:01:14 +0800

    增加 添加 单个 层表

--- GoatAutoCAD/db/GoatDB.cs
+++ GoatAutoCAD/db/GoatDB.cs
@@ -11,7 +11,7 @@ namespace GoatAutoCAD.db
         /// 将块表记录加入到块表中
         /// </summary>
         /// <returns></returns>
-        public static ObjectId AddBlockTableRecord(BlockTableRecord btr ,Database db)
+        public static ObjectId AddBlockTableRecord(BlockTableRecord btr, Database db)
         {
             ObjectId id = new ObjectId();
             using (Transaction transaction = db.TransactionManager.StartTransaction())
@@ -24,28 +24,30 @@ namespace GoatAutoCAD.db
             return id;
         }
 
-        public static ObjectId AddToModelSpace(Entity ent) {
-            ObjectId entId;
+        public static ObjectId AddToModelSpace(Entity ent)
+        {
+            ObjectId id = new ObjectId();
             using (Transaction trans = db.TransactionManager.StartTransaction())
             {
                 //③ 以读方式打开图形数据库的块表 ： 默认有 模型空间、布局1、布局2
-                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);
+                BlockTable bt = (BlockTable) trans.GetObject(db.BlockTableId, OpenMode.ForRead);
                 //④以写方式打开模型空间块表记录.打开一个存储实体的块表记录(通常绘图都在模型空间进行)，所有模型空间的实体都存储在块表的“模型空间”记录中
                 BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
                 //⑤将图形对象的信息添加到块表记录中,并返回对象的ID号.
-                entId = btr.AppendEntity(ent);
+                id = btr.AppendEntity(ent);
                 trans.AddNewlyCreatedDBObject(ent, true); //把对象添加到事务处理中.
                 trans.Commit(); //提交事务处理
             }
-            return entId;
+            return id;
         }
-        
-        public static List<ObjectId> AddToModelSpace(List<Entity> ents) {
+
+        public static List<ObjectId> AddToModelSpace(List<Entity> ents)
+        {
             List<ObjectId> list = new List<ObjectId>(ents.Count);
             using (Transaction trans = db.TransactionManager.StartTransaction())
             {
                 //③ 以读方式打开图形数据库的块表 ： 默认有 模型空间、布局1、布局2
-                BlockTable bt = (BlockTable)trans.GetObject(db.BlockTableId, OpenMode.ForRead);
+                BlockTable bt = (BlockTable) trans.GetObject(db.BlockTableId, OpenMode.ForRead);
                 //④以写方式打开模型空间块表记录.打开一个存储实体的块表记录(通常绘图都在模型空间进行)，所有模型空间的实体都存储在块表的“模型空间”记录中
                 BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
                 //⑤将图形对象的信息添加到块表记录中,并返回对象的ID号.
@@ -56,8 +58,36 @@ namespace GoatAutoCAD.db
                 }
                 trans.Commit(); //提交事务处理
             }
+
             return list;
         }
 
+
+
+        // 添加 单个 层表
+        public static ObjectId addLayerTableR1ecord(LayerTableRecord ltr)
+        {
+            ObjectId id = new ObjectId();
+            // 获取当前文档和数据库，并启动事务；
+            using (Transaction acTrans = db.TransactionManager.StartTransaction())
+            {
+                // 返回当前数据库的图层表
+                LayerTable acLyrTbl = acTrans.GetObject(db.LayerTableId,OpenMode.ForWrite) as LayerTable;
+                // 检查图层表里是否有图层 MyLayer
+                if (!acLyrTbl.Has(ltr.Name))
+                {
+                    // 以写模式打开图层表
+                    acLyrTbl.UpgradeOpen();
+                    // 添加新的图层表记录到图层表，添加事务
+                    id = acLyrTbl.Add(ltr);
+                    acTrans.AddNewlyCreatedDBObject(ltr, true);
+                    //提交修改
+                    acTrans.Commit();
+                }
+                // 关闭事务，回收内存；
+                return id;
+            }
+
+        }
     }
 }
---
+++ GoatAutoCAD/layer/GoatLayer.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using GoatAutoCAD;
+using GoatAutoCAD.db;
+
+[assembly: CommandClass(typeof(GoatLayer))]
+namespace GoatAutoCAD
+{
+
+    public class GoatLayer
+    {
+        // 命令行消息提示
+        [CommandMethod("MyGroup", "layer1", "layer1", CommandFlags.Modal)]
+        public void msg1()
+        {
+            // 新创建一个图层表记录，并命名为”MyLayer”
+            LayerTableRecord ltr = new LayerTableRecord();
+            ltr.Name = "luck";
+            GoatDB.addLayerTableR1ecord(ltr);
+        }
+
+
+
+
+    }
+
+}
