d808b90692b9670f4f7d5ccc59caf1dedc922128
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-12-17 11:50:32 +0800

    增加 封装和代码格式优化

--- GoatAutoCAD/selector/GoatSelectorUtil.cs
+++ GoatAutoCAD/selector/GoatSelectorUtil.cs
@@ -1,56 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using GoatAutoCAD.baseutil;
+using GoatAutoCAD.db;
 
 namespace GoatAutoCAD.selector
 {
-    public class GoatSelectorUtil : BaseData
-    {
+    public class GoatSelectorUtil : BaseData{
+
         /// <summary>
         /// 提示用户选择单个实体
         /// </summary>
         /// <param name="msg">选择提示</param>
         /// <returns>实体对象</returns>
-        public static Entity select(string msg)
-        {
-            Entity entity = null;
+        public static Entity select(string msg){
             PromptEntityResult ent = ed.GetEntity(msg);
-            if (ent.Status == PromptStatus.OK)
-            {
-                using (Transaction transaction = db.TransactionManager.StartTransaction())
-                {
-                    entity = (Entity)transaction.GetObject(ent.ObjectId, OpenMode.ForWrite, true);
-                    transaction.Commit();
-                }
+            if (ent.Status == PromptStatus.OK) {
+                return GoatDB.getEntityById(ent.ObjectId);
             }
-            return entity;
+            return null;
         }
 
-        public static void Select2()
-        {
+        /// <summary>
+        ///     选择集
+        /// </summary>
+        public static void Select2() {
             PromptSelectionOptions selectionOp = new PromptSelectionOptions();
             PromptSelectionResult ssRes = ed.GetSelection(selectionOp);
-            if (ssRes.Status == PromptStatus.OK)
-            {
+            if (ssRes.Status == PromptStatus.OK) {
                 SelectionSet SS = ssRes.Value;
                 int nCount = SS.Count;
                 ed.WriteMessage("选择了{0}个实体" , nCount);
             }
         }
 
-        public static void Select3()
-        {
+        public static void Select3(){
             PromptSelectionOptions selectionOp = new PromptSelectionOptions();
             //创建选择集过滤器，只选择块对象
             TypedValue[] filList = new TypedValue[1];
             filList[0] = new TypedValue((int)DxfCode.Start,"INSERT");
             SelectionFilter filter = new SelectionFilter(filList);
             PromptSelectionResult ssRes = ed.GetSelection(selectionOp, filter);
-            if (ssRes.Status == PromptStatus.OK)
-            {
+            if (ssRes.Status == PromptStatus.OK){
                 SelectionSet SS = ssRes.Value;
                 int nCount = SS.Count;
                 ed.WriteMessage("选择了{0}个块" , nCount);
@@ -63,18 +53,14 @@ namespace GoatAutoCAD.selector
         /// 选择集合
         /// </summary>
         /// <returns></returns>
-        public static DBObjectCollection selects()
-        {
+        public static DBObjectCollection selects(){
             Entity entity;
             DBObjectCollection EntityCollection = new DBObjectCollection();
             PromptSelectionResult ents = ed.GetSelection();
-            if (ents.Status == PromptStatus.OK)
-            {
-                using (Transaction trans = db.TransactionManager.StartTransaction())
-                {
+            if (ents.Status == PromptStatus.OK){
+                using (Transaction trans = db.TransactionManager.StartTransaction()){
                     SelectionSet SS = ents.Value;
-                    foreach (ObjectId id in SS.GetObjectIds())
-                    {
+                    foreach (ObjectId id in SS.GetObjectIds()){
                         entity = (Entity)trans.GetObject(id, OpenMode.ForWrite, true);
                         if (entity != null)
                             EntityCollection.Add(entity);
---
+++ GoatAutoCAD/selector/README.md
@@ -0,0 +1,15 @@
+# 选择集处理
+
+    方法 描述
+    GetSelection             用户选择
+    SelectAll               全部选中
+    SelectCrossingPolygon   相交多边形选取模式
+    SelectCrossingWindow    相交窗口选取模式
+    SelectFence              Fence 选取模式
+    SelectImplied           用户指定
+    SelectLast               选择最后一个实体
+    SelectPrevious          选择上一个选择集
+    SelectWindow            窗口选取模式
+    SelectWindowPolygon     多边形选取模式
+    
+    
